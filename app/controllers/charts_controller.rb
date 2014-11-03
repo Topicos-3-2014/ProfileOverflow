1c7119360789e33f0f580ee5eb82323fb5a2038e
Matheus Campos Cardoso <its.matheus3@gmail.com>
2014-11-03 06:57:11 +0000

    Adding piechart for age

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -2,5 +2,20 @@ class ChartsController < ApplicationController
   def index
     @top_users = User.top_users
     @top_users_chart_data = @top_users.map {|u| [u.display_name, u.reputation]}
+    
+    
+    ages = []
+    
+    User.top_users.each do |tops|
+      ages << tops.age
+    end
+    
+    
+    h = Hash.new(0)
+    ages.each { | v | h.store(v, h[v]+1) }
+    puts h
+    
+    @top_users_age_data = h
+    
   end
 end
